e2515a9444d2f4e4ffff732292187cbeb5a1320f
Kenshi <james85529@gmail.com>
2024-02-04 17:26:50 +0800

    feat: add basic element

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    <div className="app">
+      <Navigation />
+      <div>Page</div>
+      <main></main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
